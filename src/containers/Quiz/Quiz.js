e7ddd179f8d1e0d03d9136f44727a8bfa79d3809
Vasily Dmitriev <vasya.lake@bk.ru>
2022-12-06 12:39:21 +0300

    Initial Commit

---
+++ src/containers/Quiz/Quiz.js
@@ -0,0 +1,121 @@
+import React, {Component} from 'react'
+import classes from './Quiz.module.css'
+import ActiveQuiz from '../../components/ActiveQuiz/ActiveQuiz'
+import FinishedQuiz from '../../components/FinishedQuiz/FinishedQuiz'
+
+class Quiz extends Component {
+  state = {
+    results: {}, // {[id]: success error}
+    isFinished: false,
+    activeQuestion: 0,
+    answerState: null, // { [id]: 'success' 'error' }
+    quiz: [
+        {
+            question: 'The first-ever Olympic game was held here:',
+            rightAnswerId: 1,
+            answers: [
+                {text: 'Athens', id: 1},
+                {text: 'Cape Town', id: 2},
+                {text: 'Canberra', id: 3},
+                {text: 'Lahore', id: 4}
+            ]
+        },
+        {
+            question: "Which country's capital is Freetown?",
+            rightAnswerId: 2,
+            answers: [
+                {text: 'Guinea', id: 1},
+                {text: 'Sierra Leone', id: 2},
+                {text: 'Istanbul', id: 3},
+                {text: "Côte d'Ivoire", id: 4}
+            ]
+        }
+    ]
+  }
+
+  onAnswerClickHandler = answerId => {
+    if (this.state.answerState) {
+      const key = Object.keys(this.state.answerState)[0]
+      if (this.state.answerState[key] === 'success') {
+        return
+      }
+    }
+
+    const question = this.state.quiz[this.state.activeQuestion]
+    const results = this.state.results
+
+    if (question.rightAnswerId === answerId) {
+      if (!results[question.id]) {
+        results[question.id] = 'success'
+      }
+
+      this.setState({
+        answerState: {[answerId]: 'success'},
+        results
+      })
+
+      const timeout = window.setTimeout(() => {
+        if (this.isQuizFinished()) {
+          this.setState({
+            isFinished: true
+          })
+        } else {
+          this.setState({
+            activeQuestion: this.state.activeQuestion + 1,
+            answerState: null
+          })
+        }
+        window.clearTimeout(timeout)
+      }, 1000)
+    } else {
+      results[question.id] = 'error'
+      this.setState({
+        answerState: {[answerId]: 'error'},
+        results
+      })
+    }
+  }
+
+  isQuizFinished() {
+    return this.state.activeQuestion + 1 === this.state.quiz.length
+  }
+
+  retryHandler = () => {
+    this.setState({
+      activeQuestion: 0,
+      answerState: null,
+      isFinished: false,
+      results: {}
+    })
+  }
+
+  render() {
+    return (
+      <div className={classes.Quiz}>
+        <div className={classes.QuizWrapper}>
+          <h1>Ответьте на все вопросы</h1>
+
+          {
+            this.state.isFinished
+             ? <FinishedQuiz
+                  results={this.state.results}
+                  quiz={this.state.quiz}
+                  onRetry={this.retryHandler}
+                />
+             : <ActiveQuiz
+                answers={this.state.quiz[this.state.activeQuestion].answers}
+                question={this.state.quiz[this.state.activeQuestion].question}
+                onAnswerClick={this.onAnswerClickHandler}
+                quizLength={this.state.quiz.length}
+                answerNumber={this.state.activeQuestion + 1}
+                state={this.state.answerState}
+              />
+          }
+        </div>
+      </div>
+    )
+  }
+}
+
+
+export default Quiz
